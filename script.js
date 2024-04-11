ad08dcfd2e5efd2076d25a6d8c59f01b45e75d82
canadianrefrigeration <canadianrefrigeration@telus.net>
2024-04-11 11:43:57 -0700

    Fixed navsidebar issue

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+document.addEventListener('DOMContentLoaded', checkDarkModePreference);
+window.addEventListener("resize", navSideBarVisibility);
+
 function toggleMode() {
     const body = document.querySelector('body');
     body.classList.toggle('dark-mode');
     let toggleBtn = document.getElementById("toggleBtn");
     if (body.classList.contains('dark-mode')) {
         localStorage.setItem('mode', 'dark');
-        toggleBtn.textContent="White Mode";
+        toggleBtn.textContent = "White Mode";
     }
     else {
         localStorage.setItem('mode', 'white');
-        toggleBtn.textContent="Dark Mode";
+        toggleBtn.textContent = "Dark Mode";
     }
 }
 
@@ -19,21 +22,34 @@ function checkDarkModePreference() {
     if (prefersDarkScheme.matches || savedMode === 'dark') {
         body.classList.add('dark-mode');
         let toggleBtn = document.getElementById("toggleBtn");
-        toggleBtn.textContent="White Mode";
+        toggleBtn.textContent = "White Mode";
     }
 }
-
-document.addEventListener('DOMContentLoaded', checkDarkModePreference);
 
 window.onscroll = function () { scrollCheck() };
 
 function scrollCheck() {
     let navLeftBar = document.getElementById("navLeftBar");
-    if (document.body.scrollTop > document.getElementById('companyHeader').offsetHeight || document.documentElement.scrollTop > document.getElementById('companyHeader').offsetHeight) navLeftBar.classList.remove("hidden");
-    else navLeftBar.classList.add("hidden");
+    if (document.documentElement.clientWidth > 480) {
+        if (document.body.scrollTop > document.getElementById('companyHeader').offsetHeight || document.documentElement.scrollTop > document.getElementById('companyHeader').offsetHeight) navLeftBar.classList.remove("hidden");
+        else navLeftBar.classList.add("hidden");
+    }
 }
 
 function scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-} 
+}
+
+function navSideBarVisibility() {
+    let navLeftBar = document.getElementById("navLeftBar");
+    let toggleBtn = document.getElementById("toggleBtn");
+    if (document.documentElement.clientWidth > 470) {
+        navLeftBar.classList.remove("hidden");
+        toggleBtn.classList.add("fixed");
+    }
+    else {
+        navLeftBar.classList.add("hidden");
+        toggleBtn.classList.remove("fixed");
+    }
+}
